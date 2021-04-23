61744f9bd4be19d0393fb5356d496e7c294b9227
isaacwrk <isaacsouzawrk@gmail.com>
2021-04-22 23:55:35 -0300

    inserindo a function addLine()

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+
+
 let fields = document.querySelectorAll("#form-user-create [name]")
 //Criando o JSON
 let user = {}
+
+function addLine(dataUser){
+    let tr = document.createElement("tr")
+    tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `
+    document.getElementById('table-users').appendChild(tr)
+
+}
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
     //cancelar o comportamento padrão deste evento.
@@ -13,6 +34,6 @@ document.getElementById("form-user-create").addEventListener("submit", function(
             user[field.name] = field.value
         }
     })
-    console.log(user)
+    addLine(user)
     
 })
