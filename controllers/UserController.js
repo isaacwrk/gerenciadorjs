2c7f826fd5847e1647ee473a8540bd63c5f3caa8
isaacwrk <isaacsouzawrk@gmail.com>
2021-04-25 17:16:42 -0300

    Ajustando a view Admin e ajustes na promise quando o campo de filereader for nulo

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -43,7 +43,14 @@ class UserController{
         fileReader.onerror = (e)=>{
             reject(e)
         }
-        fileReader.readAsDataURL(file)
+
+        if(file){
+            fileReader.readAsDataURL(file)
+        } else{
+            //Se não tiver nenhuma imagem coloca a imagem cinza como default
+            resolve('dist/img/boxed-bg.jpg')
+        }
+        
         })
     }
 
@@ -53,6 +60,8 @@ class UserController{
         [...this.formEl.elements].forEach(function(field, index){
             if(field.name == "gender" && field.checked == true){
                 user[field.name] = field.value
+            }else if(field.name === "admin"){
+                user[field.name] = field.checked
             }else{
                 user[field.name] = field.value
             }
@@ -71,19 +80,21 @@ class UserController{
             
     }
      addLine(dataUser){
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr')
+        tr.innerHTML =
+        `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin)?'Sim' : 'Não'}</td>
             <td>${dataUser.birth}</td>
             <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
-        `
+        
+        `;
+        this.tableEl.appendChild(tr)
     }
 
 }
