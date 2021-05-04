4ffb18a3fac2ee3c224400e49436cadb792f65b6
isaacwrk <isaacsouzawrk@gmail.com>
2021-05-03 22:20:09 -0300

    [FIX]Ajustes na edicao de dados type radio e checkbox

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -134,8 +134,25 @@ class UserController{
                 let field = form.querySelector("[name="+ name.replace("_","")+"]")
 
                 if(field){
-                    if(field.type == 'file') continue;
-                    field.value = json[name]
+                    
+                    switch(field.type){
+                        case 'file':
+                            continue;
+                        break;
+
+                        case 'radio':
+                            field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            console.log(field);
+                            field.checked = true; 
+                            break;
+
+                        case 'checkbox':
+                            field.checked = json[name]
+                        break;
+
+                        default:
+                            field.value = json[name]
+                    }
                 }
                 
             }
