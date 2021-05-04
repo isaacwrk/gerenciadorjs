b50d12f778483565935d1fdf589b6e5f8b273ba4
isaacwrk <isaacsouzawrk@gmail.com>
2021-05-03 21:43:09 -0300

    [ADD][FIX] Inserindo painels de editar e cancelar dos dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,13 @@ class UserController{
         this.tableEl = document.getElementById(tableId)
 
         this.onSubmit();
+        this.onEditCancel()
+    }
+
+    onEditCancel(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate()
+        })
     }
 
     onSubmit(){
@@ -112,13 +119,29 @@ class UserController{
             <td>${(dataUser.admin)?'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger  btn-xs btn-flat">Excluir</button>
             </td>
         
         `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+            console.log(JSON.parse(tr.dataset.user))
+            this.showPanelUpdate()
+        })
+
         this.tableEl.appendChild(tr)
         this.updateCount()
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-create").style.display = "block"
+        document.querySelector("#box-user-update").style.display = "none"
+    }
+
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none"
+        document.querySelector("#box-user-update").style.display = "block"
     }
 
     updateCount(){
