cac005ce49a836b1972c62345cc20fc20a470052
isaacwrk <isaacsouzawrk@gmail.com>
2021-05-05 17:13:48 -0300

    [ADD] inserindo metodo de excluir

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -169,7 +169,7 @@ class UserController{
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
         <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger  btn-xs btn-flat">Excluir</button>
+        <button type="button" class="btn btn-danger  btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         
         `;
@@ -181,6 +181,15 @@ class UserController{
     }
 
     addEventsTr(tr){
+
+        tr.querySelector(".btn-delete").addEventListener("click", e=>{
+
+            if(confirm("Deseja realmente Excluir?")){
+                tr.remove()
+            }
+        });
+
+
         tr.querySelector(".btn-edit").addEventListener("click", e=>{
 
             let json = JSON.parse(tr.dataset.user)
