373234453c77de74c58ede7642b0b7b24ae035e8
isaacwrk <isaacsouzawrk@gmail.com>
2021-05-26 23:30:53 -0300

    [FIX] alterando de sessionStorage para localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -161,8 +161,8 @@ class UserController{
     getUsersStorage(){
         let users = []
 
-        if(sessionStorage.getItem("users")){
-            users = JSON.parse(sessionStorage.getItem("users"))
+        if(localStorage.getItem("users")){
+            users = JSON.parse(localStorage.getItem("users"))
         }
         return users
     }
@@ -184,8 +184,8 @@ class UserController{
         let users = this.getUsersStorage()
 
         users.push(data)
-
-        sessionStorage.setItem("users",JSON.stringify(users))
+        //alterando de sessionStorage para localStorage
+        localStorage.setItem("users",JSON.stringify(users))
     }
 
      addLine(dataUser){
