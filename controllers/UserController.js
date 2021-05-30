0d7241cda5f3c4bdb4ac7012030302cc1c5a917e
isaacwrk <isaacsouzawrk@gmail.com>
2021-05-30 19:27:51 -0300

    Refatorando template de editar os registros

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -38,21 +38,10 @@ class UserController{
                 result._photo = content
             }
 
-            tr.dataset.user = JSON.stringify(result)
+        let user = new User()
+        user.loadFromJSON(result)
 
-            tr.innerHTML = `
-            <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-            <td>${result._name}</td>
-            <td>${result._email}</td>
-            <td>${(result._admin)?'Sim' : 'Não'}</td>
-            <td>${Utils.dateFormat(result._register)}</td>
-            <td>
-        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger  btn-xs btn-flat">Excluir</button>
-            </td>
-        `;
-
-        this.addEventsTr(tr)
+        this.getTr(user, tr)
         this.updateCount()
         this.formUpdateEl.reset()
         btn.disabled = false
@@ -189,11 +178,15 @@ class UserController{
     }
 
      addLine(dataUser){
-        let tr = document.createElement('tr')
+        let tr = this.getTr(dataUser)
+        this.tableEl.appendChild(tr)
+        this.updateCount()
+    }
 
+    getTr(dataUser, tr = null){
+        if(tr === null ) tr = document.createElement('tr')
         //convertendo objeto em string e guardando em JSON
         tr.dataset.user = JSON.stringify(dataUser)
-
         tr.innerHTML =
         `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -209,9 +202,7 @@ class UserController{
         `;
 
         this.addEventsTr(tr)
-
-        this.tableEl.appendChild(tr)
-        this.updateCount()
+        return tr
     }
 
     addEventsTr(tr){
